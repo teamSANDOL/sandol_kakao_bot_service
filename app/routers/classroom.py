8aa2666acac51e9b18043c00f288a1096e3284d8
Seokyoung-Hong <tjrdud0330@naver.com>
2025-05-28 22:10:23 +0900

    feat: 현재 빈 강의실 조회 기능 추가

--- app/routers/classroom.py
+++ app/routers/classroom.py
@@ -15,7 +15,7 @@ from kakao_chatbot.response.components import (
 
 from app.config import logger
 from app.schemas.classroom import Classroom, EmptyClassroomInfo
-from app.services.classroom_timetable_serivce import search_empty_classroom_by_time
+from app.services.classroom_timetable_serivce import search_empty_classroom_by_time, search_empty_classroom_now
 from app.utils import create_openapi_extra
 from app.utils.auth_client import get_service_xuser_client
 from app.utils.http import XUserIDClient
@@ -23,6 +23,7 @@ from app.utils.kakao import parse_payload
 from app.utils.classroom import make_empty_classroom_components
 
 classroom_router = APIRouter(prefix="/classroom")
+
 
 @classroom_router.post(
     "/empty/time",
@@ -73,6 +74,25 @@ async def empty_classroom_by_time(
     )
 
     empty_classrooms = await search_empty_classroom_by_time(client, day, start_time, end_time)
+
+    components = make_empty_classroom_components(empty_classrooms)
+
+    return JSONResponse(KakaoResponse(
+        component_list=components
+    ).get_dict()
+    )
+
+
+async def empty_classroom_now(
+    client: Annotated[XUserIDClient, Depends(get_service_xuser_client)],
+):
+    """현재 빈 강의실을 조회합니다.
+
+    현재 시간에 빈 강의실을 조회하여 리스트 카드 형태로 반환합니다.
+    """
+    logger.info("현재 빈 강의실 조회 called")
+
+    empty_classrooms = await search_empty_classroom_now(client)
 
     components = make_empty_classroom_components(empty_classrooms)
 
